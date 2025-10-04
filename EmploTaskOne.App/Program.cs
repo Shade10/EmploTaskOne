c18c9a4e68474185b16c09edf750df93869c27ac
Marcin Kass <marcin.kass10@gmail.com>
2025-10-04 15:17:27 +0200

    delete unused usings

--- EmploTaskOne.App/Program.cs
+++ EmploTaskOne.App/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmploTaskOne.App
+﻿namespace EmploTaskOne.App
 {
     internal class Program
     {
--- EmploTaskOne.Domain/Interfaces/IEmployeeRepository.cs
+++ EmploTaskOne.Domain/Interfaces/IEmployeeRepository.cs
@@ -1,9 +1,5 @@
 ﻿using EmploTaskOne.Domain.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EmploTaskOne.Domain.Interfaces
 {
